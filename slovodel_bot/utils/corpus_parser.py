5542377fc17f1148ebadc45c4eeb63f6d5b07f03
Simon Weiss <git@simonweiss.space>
2020-08-01 13:43:28 +0300

    ✨ corpus_parser ready

---
+++ slovodel_bot/utils/corpus_parser.py
@@ -0,0 +1,48 @@
+"""
+CLI corpus parser for slovodel_bot.
+
+Takes Russian dictionary text file with one line per wors as input
+and produces a JSON file with syllable data, that could be fed to markovify library.
+
+All symbols are ignored exept Russian letters and dashes.
+All letters are converted to lowercase.
+To get clean results each dictionary should contain only words of given type,
+(nouns, adjectives or verbs).
+
+Usage:
+    $ python3 corpus_parser.py input_file output_file
+
+Example:
+    $ python3 corpus_parser.py \
+        ~/data/corpus/rusdict_nouns.txt ../resources/rusdict_nouns.json
+"""
+import click
+from markovify import Chain
+from rusyll import rusyll
+
+
+@click.command()
+@click.argument('input_file', type=click.File('r'))
+@click.argument('output_file', type=click.File('w+'))
+def main(input_file, output_file):
+    """Makes things happen."""
+    corpus = []
+    lines_processed = 0
+
+    for line in input_file:
+        lines_processed += 1
+        try:
+            corpus.append(
+                rusyll.word_to_syllables_safe_wd(line.strip().lower())
+            )
+        except AssertionError:
+            pass
+
+    chain = Chain(corpus, 1)
+    output_file.write(chain.to_json())
+
+    print("{} lines processed. {} words added.".format(lines_processed, len(corpus)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/utils/test_corpus_parser.py
@@ -0,0 +1,33 @@
+import os
+from click.testing import CliRunner
+from markovify import Chain
+from slovodel_bot.utils import corpus_parser
+
+
+def test_main(tmpdir):
+    path = tmpdir.mkdir("sub")
+    input_file = path.join("test_dict_1.txt")
+    output_file = path.join("test_json_1.json")
+
+    input_file.write(
+        "Аба\n" + \
+        "аБаба \n" + \
+        "аб-ба\n" + \
+        "zвеве\n" + \
+        "ве ве\n" + \
+        "ве.\n"
+    )
+
+    corpus = [
+        ['а', 'ба'],
+        ['а', 'ба', 'ба'],
+        ['аб', '-', 'ба'],
+    ]
+    chain = Chain(corpus, 1)
+
+    runner = CliRunner()
+    result = runner.invoke(corpus_parser.main, [str(input_file), str(output_file)])
+    assert "processed" in result.output
+    assert result.exit_code == 0
+    with open(str(output_file), 'r') as test_json_1:
+        assert test_json_1.read() == chain.to_json()

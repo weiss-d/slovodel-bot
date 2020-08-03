7b9bcf9d486c0f5c86f784f15cfbaa08698bf044
Simon Weiss <git@simonweiss.space>
2020-08-03 09:28:01 +0300

    Nox session dependencies mangement via Poetry

--- noxfile.py
+++ noxfile.py
@@ -2,22 +2,38 @@ import tempfile
 
 import nox
 
+
 nox.options.sessions = "lint", "tests"
 
 locations = "slovodel_bot", "tests", "noxfile.py"
 
 
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-bugbear", "flake8-import-order")
+    install_with_constraints(
+        session, "flake8", "flake8-black", "flake8-bugbear", "flake8-import-order"
+    )
     session.run("flake8", *args)
 
 
 @nox.session(python="3.8")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -33,11 +49,15 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest", "--cov")
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock", "fakeredis"
+    )
+    session.run("pytest", *args)

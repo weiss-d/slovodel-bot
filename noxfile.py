3e4f1c5214388bf3b235038a086af3ddebb1e567
Simon Weiss <git@simonweiss.space>
2020-08-04 10:41:00 +0300

    Added mypy to Nox sessions

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import tempfile
 import nox
 
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "mypy", "tests"
 
 locations = "slovodel_bot", "tests", "noxfile.py"
 
@@ -19,6 +19,13 @@ def install_with_constraints(session, *args, **kwargs):
             external=True,
         )
         session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session(python=["3.8", "3.7"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
 
 
 @nox.session(python=["3.8", "3.7"])
